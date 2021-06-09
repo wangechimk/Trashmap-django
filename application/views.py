69d14824c5bd2752197a2f0c34f13f172ea020b7
wangechimk <wangechik.michelle@gmail.com>
2021-06-09 23:27:44 +0300

    create views for registration and login

--- application/views.py
+++ application/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from django.contrib.auth import get_user_model,logout
+from django.core.exceptions import ImproperlyConfigured
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework.views import APIView
+from .utils import get_and_authenticate_user, create_user_account
+from . import serializers
+from django.conf import settings
+
+User = get_user_model()
+
 
 # Create your views here.
+class AuthViewSet(viewsets.GenericViewSet):
+    permission_classes = [AllowAny, ]
+    serializer_class = serializers.EmptySerializer
+    serializer_classes = {
+        'login': serializers.UserLoginSerializer,
+    }
+
+    @action(methods=['POST', ], detail=False)
+    def login(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = get_and_authenticate_user(**serializer.validated_data)
+        data = serializers.AuthUserSerializer(user).data
+        return Response(data=data, status=status.HTTP_200_OK)
+    
+    @action(methods=['POST', ], detail=False)
+    def register(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = create_user_account(**serializer.validated_data)
+        data = serializers.AuthUserSerializer(user).data
+        return Response(data=data, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST', ], detail=False)
+    def logout(self, request):
+        logout(request)
+        data = {'success': 'Sucessfully logged out'}
+        return Response(data=data, status=status.HTTP_200_OK)
+    
+
+    def get_serializer_class(self):
+        if not isinstance(self.serializer_classes, dict):
+            raise ImproperlyConfigured("serializer_classes should be a dict mapping.")
+
+        if self.action in self.serializer_classes.keys():
+            return self.serializer_classes[self.action]
+        return super().get_serializer_class()
+
+class Registration(APIView):
+    def post(self,request):
+        serializer=serializers.CreateUserSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response({'success':'user was registered successfully!'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
