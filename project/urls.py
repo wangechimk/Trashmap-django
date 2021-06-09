47b6149d6de2336c62ffddcf16b98b9103704d9d
wangechimk <wangechik.michelle@gmail.com>
2021-06-09 23:26:37 +0300

    add url endpoints

--- project/urls.py
+++ project/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.urls import path,include
 from django.urls import path
+
+app_name='application'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('application.urls', namespace='application')),
+   
 ]
+
