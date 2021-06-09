d41d205b3a09d7ddf9b1e57cc8564d1db966af38
wangechimk <wangechik.michelle@gmail.com>
2021-06-09 23:25:53 +0300

    create the user model

--- application/models.py
+++ application/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    location = models.CharField( max_length=255, blank=True, null=False)
+    
